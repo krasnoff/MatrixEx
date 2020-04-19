f2effeb3b62a1751084ab6c5b61c94d6ca08c59e
kobi krasnoff - desktop <krasnoff.kobi@gmail.com>
2020-04-19 15:47:35 +0300

    validation client side

--- ClientApp/src/app/sign-up/sign-up.component.ts
+++ ClientApp/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    password: new FormControl(''),
+    passwordRepeat: new FormControl(''),
+  });
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 }
